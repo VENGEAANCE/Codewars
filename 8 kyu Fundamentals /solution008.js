da309371a685ce8c1b9843f4e81eed46340989a2
Jared <jared@Mac-mini.local>
2022-05-28 19:22:35 -0400

    8 Kyu

---
+++ 8 kyu Fundamentals /solution008.js
@@ -0,0 +1,23 @@
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Y
+const words = ['I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all']
+
+function nbPetals(n) {
+    return words[(n - 1) % words.length]
+}
+
+const howMuchIloveYou = n => words[(n - 1) % words.length]
