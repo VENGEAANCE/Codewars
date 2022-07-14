7f0f5c47294e11b26ef45b3acb02ba51e8cfab7e
Jared Machado <102765710+VENGEAANCE@users.noreply.github.com>
2022-07-14 18:09:44 -0400

    Delete solution16.js

--- 8 kyu Fundamentals /solution16.js
+++
@@ -1,8 +0,0 @@
-// Rock Paper Scissors
-// Let's play! You have to return which player won! In case of a draw return Draw!.
-
-const rps = (p1, p2) => {
-    return p1 === p2 ? 'Draw!'
-        : p1 === 'scissors' && p2 === 'paper' || p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock' ? 'Player 1 won!'
-            : 'Player 2 won!'
-}
