04c1dc8efd1d226168a3511ca1f6346535225cc3
Jared Machado <102765710+VENGEAANCE@users.noreply.github.com>
2022-07-30 20:38:30 -0400

    Update solution34.js

--- 8 kyu Fundamentals /solution34.js
+++ 8 kyu Fundamentals /solution34.js
@@ -1,3 +1,5 @@
+#Task
+
 // Create a function with two arguments that will return an array of the first (n) multiples of (x).
 
 // Assume both the given number and the number of times to count will be positive numbers greater than 0.
@@ -10,6 +12,7 @@
 // countBy(2,5) === [2,4,6,8,10]
 
 
+#Solution
 
 function countBy(x, n) {
     var z = [];
