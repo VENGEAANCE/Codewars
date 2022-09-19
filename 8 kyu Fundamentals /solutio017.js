90be6336af978aeac5f128f0d725f91cd21acaef
Jared <jaredterrospin@gmail.com>
2022-09-18 20:56:27 -0400

    8 Kyu

--- 8 kyu Fundamentals /solutio017.js
+++ 8 kyu Fundamentals /solutio017.js
@@ -1,4 +1,4 @@
-// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive )
 
 function powersOfTwo(n) {
     let result = [];
